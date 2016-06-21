bb4c0c0971a548c759d94613833ec1fe7a3d1c44
EvertonQuadros <Not275ssd@192.168.6.30>
2016-06-21 19:59:26 -0300

    Criação da classe DAO para comunicação e testes da classe modelo Login com o banco de dados;

---
+++ Gabinete/src/java/br/edu/ifrs/modelo/dao/LoginDAO.java
@@ -0,0 +1,73 @@
+package br.edu.ifrs.modelo.dao;
+
+import br.edu.ifrs.modelo.bean.Login;
+import br.edu.ifrs.util.Conexao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/** 
+ * Document   : LoginDAO.java
+ * Created on : 21/06/2016, 17:43
+ * Author     : EvertonQuadros
+ * Escopo     : Eventos/Projeto
+ * Descricao  : Classe DAO que busca dados do login.
+ * Observações: Alterar com aviso prévio, comentar revisões.
+ */
+
+public class LoginDAO {
+    
+    public static Login consultar(String login, String senha) throws Exception {
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        Login p = new Login();
+        try {
+            try {
+                
+                con = Conexao.abrirConexao();
+                /* Preprar a sentença SQL */
+                pstmt = con.prepareStatement("select * from perfis where login = ? and senha = ?");
+                pstmt.setString(1, login);
+                pstmt.setString(2, senha);
+                /* Executar a sentença no banco de dados */
+                rs = pstmt.executeQuery();
+                if (rs.next() == true) {
+                    
+                    p.setId(rs.getInt("id"));
+                    p.setLogin(rs.getString("login"));
+                    p.setSenha(rs.getString("senha"));
+                    
+                }
+                
+                if(!p.getLogin().equals("")){
+                    return p;
+                }
+                
+            } catch (Exception e) {
+                
+                throw new Exception(e.getMessage());
+                
+            } finally {
+                
+                if(pstmt != null){
+                    
+                    pstmt.close();
+                
+                }
+                
+                if(con != null){
+                    
+                    con.close(); 
+                
+                }
+                
+            }
+        } catch (Exception ex) {
+            throw ex;
+        }
+        
+        return null;
+    }
+    
+}
