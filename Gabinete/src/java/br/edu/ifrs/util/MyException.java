e519cbfa67e0604a92b3d18461054fd1595b4a35
10070162 <10070162@LAB409-32-24899.restinga.ifrs.edu.br>
2016-07-06 10:10:13 -0300

    Duas páginas adicionadas em br.edu.ifrs.util

---
+++ Gabinete/src/java/br/edu/ifrs/util/MyException.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifrs.util;
+
+/**
+ *
+ * @author 10070155
+ */
+public class MyException extends Exception {
+
+    public MyException() {
+    }
+
+    public MyException(String message) {
+        super(message);
+    }
+
+    public MyException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public MyException(Throwable cause) {
+        super(cause);
+    }
+    
+}
---
+++ Gabinete/src/java/br/edu/ifrs/util/criacaoBanco.sql
@@ -0,0 +1,40 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  Portal
+ * Created: 05/07/2016
+ */
+
+create database trabalho_gabinete
+
+drop table espacos;
+
+create table espacos
+(
+  numero int not null,
+  nome varchar(255),
+  descricao text,
+  lotacao int,
+  tipo varchar(15),
+  projetor boolean,
+  internet boolean,
+  quadro boolean,
+  especialidade varchar(100),
+  equipamentos text,
+  situacao varchar(45),
+  primary key (numero)
+);
+
+
+insert into espacos values(400,'ola','descriçao',30,'sala de aula',true,false,true,'computação','computadores','ativo');
+
+insert into espacos values(401,'ola1','descriçao',30,'Laboratorio',true,false,true,'computação','computadores','inativo');
+
+insert into espacos values(402,'ola2','descriçao',30,'laboratorio',true,false,true,'computação','computadores','ativo');
+
+insert into espacos values(403,'ola3','descriçao',30,'sala de aula',true,false,true,'computação','computadores','ativo');
+
+select * from espacos;
